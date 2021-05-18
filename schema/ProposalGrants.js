c426d0c6e11ad0cd142fe6b8563787de6371fb76
Andrea Vanini <andrea.vanini@uidu.org>
2021-05-18 20:45:45 +0200

    Added ProposalGrants

---
+++ schema/ProposalGrants.js
@@ -0,0 +1,62 @@
+/* eslint-disable no-undef */
+import { toGlobalId } from './utils';
+
+cube('ProposalGrants', {
+  sql: 'SELECT * FROM proposal_grants',
+
+  extends: ActiveRecordModels,
+
+  joins: {
+    Proposals: {
+      relationship: 'belongsTo',
+      sql: `${CUBE}.proposal_id = ${Proposals}.id`,
+    },
+
+    Grants: {
+      relationship: 'belongsTo',
+      sql: `${CUBE}.grant_id = ${Grants}.id`,
+    },
+  },
+
+  measures: {
+    count: {
+      type: 'count',
+      drillMembers: [id, createdAt, updatedAt],
+    },
+  },
+
+  dimensions: {
+    id: {
+      shown: true,
+      sql: `${toGlobalId('ProposalGrant', `${CUBE}.id`)}`,
+      type: 'string',
+      primaryKey: true,
+    },
+
+    status: {
+      type: 'string',
+      case: {
+        when: [
+          { sql: `${CUBE}.status = 99`, label: 'discarded' },
+          { sql: `${CUBE}.status = 60`, label: 'lost' },
+          { sql: `${CUBE}.status = 50`, label: 'won' },
+          { sql: `${CUBE}.status = 40`, label: 'submitted' },
+          { sql: `${CUBE}.status = 30`, label: 'chosen' },
+          { sql: `${CUBE}.status = 20`, label: 'evaluated' },
+          { sql: `${CUBE}.status = 10`, label: 'saved' },
+          { sql: `${CUBE}.status IS NULL`, label: 'unknown' },
+        ],
+        else: { label: 'unknown' },
+      },
+    },
+  },
+
+  segments: {
+    discarded: {
+      sql: `${CUBE}.status = 99`,
+    },
+    notDiscarded: {
+      sql: `${CUBE}.status < 99`,
+    },
+  },
+});
