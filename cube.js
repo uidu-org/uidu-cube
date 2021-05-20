8baba2b7143d0f3868e94c51f1a8f6348cce89cc
Andrea Vanini <andrea.vanini@uidu.org>
2021-05-20 23:26:41 +0200

    allow connection from uidu.org

--- cube.js
+++ cube.js
@@ -2,7 +2,7 @@
 module.exports = {
   http: {
     cors: {
-      origin: [/\.uidu.local/, /\.uidu.dev$/, /localhost/],
+      origin: [/\.uidu.local/, /\.uidu.dev$/, /localhost/, /\.uidu.org$/],
     },
   },
   queryTransformer: (query, { securityContext }) => {
