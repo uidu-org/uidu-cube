3142f83ae60c7872451c3a223b932aeced7be378
Andrea Vanini <andrea.vanini@uidu.org>
2021-06-04 11:02:41 +0200

    Fix stories cube

--- schema/Stories.js
+++ schema/Stories.js
@@ -1,5 +1,10 @@
-cube(`Stories`, {
-  sql: `SELECT * FROM stories`,
+/* eslint-disable no-undef */
+import { toGlobalId } from './utils';
+
+cube('Stories', {
+  sql: 'SELECT * FROM stories',
+
+  extends: ActiveRecordModels,
 
   joins: {
     Workspaces: {
@@ -10,136 +15,81 @@ cube(`Stories`, {
 
   measures: {
     count: {
-      type: `count`,
+      type: 'count',
       drillMembers: [id, name, seoTitle, createdAt, updatedAt],
     },
 
     commentsCount: {
-      sql: `comments_count`,
-      type: `sum`,
+      sql: 'comments_count',
+      type: 'sum',
     },
 
     fbSharesCount: {
-      sql: `fb_shares_count`,
-      type: `sum`,
+      sql: 'fb_shares_count',
+      type: 'sum',
     },
 
     twSharesCount: {
-      sql: `tw_shares_count`,
-      type: `sum`,
+      sql: 'tw_shares_count',
+      type: 'sum',
     },
 
     viewsCount: {
-      sql: `views_count`,
-      type: `sum`,
+      sql: 'views_count',
+      type: 'sum',
     },
   },
 
   dimensions: {
-    abstract: {
-      sql: `abstract`,
-      type: `string`,
-    },
-
-    body: {
-      sql: `body`,
-      type: `string`,
-    },
-
-    bodyData: {
-      sql: `body_data`,
-      type: `string`,
-    },
-
-    bodyRaw: {
-      sql: `body_raw`,
-      type: `string`,
-    },
-
-    cachedStoryTagList: {
-      sql: `cached_story_tag_list`,
-      type: `string`,
-    },
-
-    cover: {
-      sql: `cover`,
-      type: `string`,
-    },
-
-    coverData: {
-      sql: `cover_data`,
-      type: `string`,
-    },
-
     id: {
-      sql: `id`,
-      type: `number`,
+      shown: true,
+      sql: `${toGlobalId('Story', `${CUBE}.id`)}`,
+      type: 'string',
       primaryKey: true,
     },
 
-    importedAlternativePermalink: {
-      sql: `imported_alternative_permalink`,
-      type: `string`,
+    abstract: {
+      sql: 'abstract',
+      type: 'string',
     },
 
-    importedHtml: {
-      sql: `imported_html`,
-      type: `string`,
-    },
+    // cachedStoryTagList: {
+    //   sql: 'cached_story_tag_list',
+    //   type: 'string',
+    // },
 
-    importedPermalink: {
-      sql: `imported_permalink`,
-      type: `string`,
+    cover: {
+      sql: `${Stories}.cover_data->>"$.derivatives.small.id"`,
+      type: 'string',
+      format: 'imageUrl',
+      meta: {
+        kind: 'cover',
+      },
     },
 
     name: {
-      sql: `name`,
-      type: `string`,
+      sql: 'name',
+      type: 'string',
     },
 
     seoKeyword: {
-      sql: `seo_keyword`,
-      type: `string`,
+      sql: 'seo_keyword',
+      type: 'string',
     },
 
     seoMetaDescription: {
-      sql: `seo_meta_description`,
-      type: `string`,
+      sql: 'seo_meta_description',
+      type: 'string',
     },
 
     seoTitle: {
-      sql: `seo_title`,
-      type: `string`,
-    },
-
-    createdAt: {
-      sql: `created_at`,
-      type: `time`,
-    },
-
-    updatedAt: {
-      sql: `updated_at`,
-      type: `time`,
-    },
-
-    featuredAt: {
-      sql: `featured_at`,
-      type: `time`,
-    },
-
-    importedAt: {
-      sql: `imported_at`,
-      type: `time`,
-    },
-
-    listedAt: {
-      sql: `listed_at`,
-      type: `time`,
+      sql: 'seo_title',
+      type: 'string',
     },
 
     publishedAt: {
-      sql: `published_at`,
-      type: `time`,
+      sql: 'published_at',
+      type: 'time',
     },
   },
 });
